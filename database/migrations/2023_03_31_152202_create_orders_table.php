83dcfe05d3dffdfd12673a605919a99f31298232
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-31 18:50:20 +0200

    added orders migration

---
+++ database/migrations/2023_03_31_152202_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('delivery_address');
+            $table->string('costumer_name', 50);
+            $table->char('customer_phone_number', 50);
+            $table->string('customer_email', 50);
+            $table->decimal('total_price', 6, 2);
+            $table->boolean('is_paid')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
