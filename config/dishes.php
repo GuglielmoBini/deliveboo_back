2abcc2b7f377271f45bc333b02e5867487c440f7
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-04-01 16:49:34 +0200

    creati array di dati in file config

---
+++ config/dishes.php
@@ -0,0 +1,348 @@
+<?php return
+// !NB FARE IL CONTROLLO NEL CASO DESCRIPTION O IMAGE NON SIANO PRESENTI 
+[
+    [
+        'restaurant_id'=>1,
+        'name'=>'margherita',
+        'description'=>'mozzarella e pomodoro',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDVDFIBNbMgQllKbIpXdB7Yhusb0iL7v8LIQ&usqp=CAU',
+        'price'=>5.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>1,
+        'name'=>'marinara',
+        'description'=>'pomodoro',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq0chTQyyPjNlEidLQ9IQ4Qm5y4kCzsBHTeQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>1,
+        'name'=>'Speck e mascarpone',
+        'description'=>'base margherita con speck e mascarpone',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqOqaY28ZhATA4hn6ZGC-bn-SUL1Vc57DxPA&usqp=CAU',
+        'price'=>10.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>1,
+        'name'=>'Capricciosa',
+        'description'=>'base margherita con funchi carciofi acciughe e olive',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg4MLGVL5pz2yV5akJB6Zf-CoOPu90YppfeQ&usqp=CAU',
+        'price'=>10.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>1,
+        'name'=>'per perosone speciali',
+        'description'=>'base margherita wurtsel e patatine',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZgXbx256EBIvAFstKdzvXr2r3f2gqnwCPeg&usqp=CAU',
+        'price'=>20.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>2,
+        'name'=>'hamburger classico',
+        'description'=>'pane hamburger, cipolle, insalata,pomodori e pancetta croccante',
+        'price'=>7.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>2,
+        'name'=>'cheesburger',
+        'description'=>'pane , hamburger, cipolle, insalata,pomodori, pancetta croccante e formaggio',
+        'price'=>7.00 ,
+        'is_visible'=>0
+    ],[
+        'restaurant_id'=>2,
+        'name'=>' duble cheesburger',
+        'description'=>'pane , 2 hamburger, cipolle, insalata,pomodori, pancetta croccante e formaggio',
+        'price'=>10.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>2,
+        'name'=>' triplo cheesburger',
+        'description'=>'pane , 3 hamburger, cipolle, insalata,pomodori, pancetta croccante e formaggio',
+        'price'=>13.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>2,
+        'name'=>' patatine fritte',
+        'price'=>5.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>2,
+        'name'=>' anelli di cipolla',
+        'price'=>13.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>2,
+        'name'=>'alette di pollo',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>3,
+        'name'=>'tomawhack',
+        'price'=>40.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>3,
+        'name'=>'filetto',
+        'price'=>30.00 ,
+        'is_visible'=>0
+    ],[
+        'restaurant_id'=>3,
+        'name'=>'sotto filetto',
+        'price'=>40.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>3,
+        'name'=>'carbonara',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDzoHF6EkddsbipA0XPVl2ANcvDEttjYbPdA&usqp=CAU',
+        'price'=>15.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>3,
+        'name'=>'amatriciana',
+        'price'=>15.00 ,
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo-MZWlIwRkK7ss-lNeYB11gBzL7FDfHVmUw&usqp=CAU',
+        'is_visible'=>1
+        
+    ],[
+        'restaurant_id'=>3,
+        'name'=>'trofie al pesto',
+        'price'=>35.00 ,
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmsWyFJr4_JWUfbbDp-dUNv99RQFRoPsSODQ&usqp=CAU',
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato al cioccolato',
+        'price'=>5.00 ,
+        'is_visible'=>0
+        
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato alla fragola',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato alla nocciola',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato al pistacchio',
+        'price'=>5.00 ,
+        'is_visible'=>0
+        
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato alla vaniglia',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato alla crema',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        
+    ],[
+        'restaurant_id'=>4,
+        'name'=>'gelato alla crema',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>5,
+        'name'=>'margherita',
+        'description'=>'mozzarella e pomodoro',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDVDFIBNbMgQllKbIpXdB7Yhusb0iL7v8LIQ&usqp=CAU',
+        'price'=>5.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>5,
+        'name'=>'marinara',
+        'description'=>'pomodoro',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq0chTQyyPjNlEidLQ9IQ4Qm5y4kCzsBHTeQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>5,
+        'name'=>'Speck e mascarpone',
+        'description'=>'base margherita con speck e mascarpone',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqOqaY28ZhATA4hn6ZGC-bn-SUL1Vc57DxPA&usqp=CAU',
+        'price'=>10.00 ,
+        'is_visible'=>0
+    ],[
+        'restaurant_id'=>5,
+        'name'=>'Capricciosa',
+        'description'=>'base margherita con funchi carciofi acciughe e olive',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg4MLGVL5pz2yV5akJB6Zf-CoOPu90YppfeQ&usqp=CAU',
+        'price'=>10.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>5,
+        'name'=>'per perosone speciali',
+        'description'=>'base margherita wurtsel e patatine',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZgXbx256EBIvAFstKdzvXr2r3f2gqnwCPeg&usqp=CAU',
+        'price'=>20.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>6,
+        'name'=>'tomawhack',
+        'price'=>40.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>6,
+        'name'=>'filetto',
+        'price'=>30.00 ,
+        'is_visible'=>0
+    ],[
+        'restaurant_id'=>6,
+        'name'=>'sotto filetto',
+        'price'=>40.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>6,
+        'name'=>'costata',
+        'price'=>20.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>7,
+        'name'=>'nighiri salmone',
+        'image'=> 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV8jNZ6i10kOLu4aRNxShsG0HO36H-WQDgFQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>7,
+        'name'=>'nighiri tonno',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN29D2zqmntxcILXSgeosiSkEt4MhCB0ljGQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>7,
+        'name'=>'nighiri gambero',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgbsWT1d9w7AHXjrQ_HUw_UNHfYw4_I2AxXw&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>7,
+        'name'=>'tempura di gamber',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYZAnzuGYW6t-Nko4d7zpJxZlcLqKM9BvKBA&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>7,
+        'name'=>'ravioli al vapore',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk8W7dt1agyc7yK507u5_Uo3yQ22mseBo5kA&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>8,
+        'name'=>'nighiri salmone',
+        'image'=> 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV8jNZ6i10kOLu4aRNxShsG0HO36H-WQDgFQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>8,
+        'name'=>'nighiri tonno',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN29D2zqmntxcILXSgeosiSkEt4MhCB0ljGQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>8,
+        'name'=>'nighiri gambero',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgbsWT1d9w7AHXjrQ_HUw_UNHfYw4_I2AxXw&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>8,
+        'name'=>'tempura di gamber',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYZAnzuGYW6t-Nko4d7zpJxZlcLqKM9BvKBA&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>8,
+        'name'=>'ravioli al vapore',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk8W7dt1agyc7yK507u5_Uo3yQ22mseBo5kA&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>9,
+        'name'=>'hamburger classico',
+        'description'=>'pane hamburger, cipolle, insalata,pomodori e pancetta croccante',
+        'price'=>7.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>9,
+        'name'=>'cheesburger',
+        'description'=>'pane , hamburger, cipolle, insalata,pomodori, pancetta croccante e formaggio',
+        'price'=>7.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>9,
+        'name'=>' duble cheesburger',
+        'description'=>'pane , 2 hamburger, cipolle, insalata,pomodori, pancetta croccante e formaggio',
+        'price'=>10.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>9,
+        'name'=>' triplo cheesburger',
+        'description'=>'pane , 3 hamburger, cipolle, insalata,pomodori, pancetta croccante e formaggio',
+        'price'=>13.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>9,
+        'name'=>' patatine fritte',
+        'price'=>5.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>9,
+        'name'=>' anelli di cipolla',
+        'price'=>13.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>9,
+        'name'=>'alette di pollo',
+        'price'=>5.00 ,
+        'is_visible'=>1
+        //!-----------------------
+    ],[
+        'restaurant_id'=>10,
+        'name'=>'nighiri salmone',
+        'image'=> 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV8jNZ6i10kOLu4aRNxShsG0HO36H-WQDgFQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>10,
+        'name'=>'nighiri tonno',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN29D2zqmntxcILXSgeosiSkEt4MhCB0ljGQ&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>10,
+        'name'=>'nighiri gambero',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgbsWT1d9w7AHXjrQ_HUw_UNHfYw4_I2AxXw&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>10,
+        'name'=>'tempura di gamber',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYZAnzuGYW6t-Nko4d7zpJxZlcLqKM9BvKBA&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ],[
+        'restaurant_id'=>10,
+        'name'=>'ravioli al vapore',
+        'image'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk8W7dt1agyc7yK507u5_Uo3yQ22mseBo5kA&usqp=CAU',
+        'price'=>4.00 ,
+        'is_visible'=>1
+    ]
+]
+?>
