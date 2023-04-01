8e2051f59154c38cb94ad194a780341438b26c48
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-04-01 15:13:11 +0200

    fixing image issue in type migration

--- database/migrations/2023_03_31_152822_create_types_table.php
+++ database/migrations/2023_03_31_152822_create_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->string('name')->required();
-            $table->image();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
