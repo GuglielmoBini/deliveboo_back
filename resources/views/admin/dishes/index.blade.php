bf0cb51af8cea392539f4403ccd13e584b213d07
NicolaPlacentino <nyko0921@gmail.com>
2023-04-03 18:53:36 +0200

    added graphics

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -7,24 +7,36 @@
         @foreach ($dishes as $dish)
         <div class="card" style="width: 18rem;">
 			@isset($dish->image)
-            <img src="{{$dish->image}}" class="card-img-top" alt="{{$dish->name}}">
+            <figure class="rounded-top">
+                <img src="{{$dish->image}}" class="card-img-top img-custom" alt="{{$dish->name}}">
+            </figure>
             @endisset
 
-            <div class="card-body">
-                <h3>{{$dish->name}}</h3>
+            <div class="card-body d-flex flex-column justify-content-between">
+                <h3 class="text-uppercase custom-text-title">{{$dish->name}}</h3>
 				@if($dish->description)
                 <p class="card-text">{{$dish->description}}</p>
 				@endif
 				<div class="d-flex justify-content-between">
-					<span>{{$dish->price}}</span>
+					<span class="text-custom-secondary">€{{$dish->price}}</span>
 					<div class="form-check form-switch">
 						<label class="form-check-label" for="flexSwitchCheckDefault">Mostra nel menù</label>
                         {{-- todo fare il form per il toggle--}}
                         <input class="form-check-input" type="checkbox" role="switch" id="isVisible">
 					</div>
 				</div>
+
+                <div class="mt-2 d-flex justify-content-end">
+                    <a href="#" class="btn btn-sm btn-custom-secondary">Modifica</a>
+                    <button href="" class="ms-2 btn btn-sm btn-custom-secondary">Elimina</button>
+                </div>
             </div>
         </div>
         @endforeach
     </div>
+
+    <div class="d-flex justify-content-end pe-5 me-5 mt-3">
+        <a href="#" class="btn btn-custom-secondary">Aggiungi Piatto</a>
+    </div>
+
 @endsection
