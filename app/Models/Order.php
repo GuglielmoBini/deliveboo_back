df8e5cabdb3a037e470a79899dc6a35ec2467a9c
GuglielmoBini <guglielmobini@gmail.com>
2023-04-03 15:13:46 +0200

    formatted date

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,7 +19,20 @@ class Order extends Model
         'is_paid',
     ];
 
-    public function dishes(){
+    public function dishes()
+    {
         return $this->belongsToMany(Dishes::class);
+    }
+
+    public function getDate($date_column, $format = 'd-m-Y')
+    {
+        $date = $this->$date_column;
+        return Carbon::create($date)->format($format);
+    }
+
+    public function getDateDiff($date_column)
+    {
+        $date = $this->$date_column;
+        return Carbon::parse($date)->diffForHumans();
     }
 }
