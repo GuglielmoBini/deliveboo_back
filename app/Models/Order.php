2bd7eb6a3c49a99e6cc04ebd268350e91effe585
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-04-03 08:56:39 +0200

    dish and order models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'delivery_address',
+        'costumer_name',
+        'customer_phone_number',
+        'customer_email',
+        'total_price',
+        'is_paid',
+    ];
+
+    public function dishes(){
+        return $this->belongsToMany(Dishes::class);
+    }
 }
