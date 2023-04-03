e5fa6b15341de95f1a203d8720ea8864e4835014
NicolaPlacentino <nyko0921@gmail.com>
2023-04-03 12:42:20 +0200

    added dish edit and update methods

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -7,6 +7,7 @@ use App\Models\Dish;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 class DishController extends Controller
 {
@@ -31,7 +32,7 @@ class DishController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Dish $dish)
     {
         $request->validate([
             'name' => 'required|string',
@@ -74,17 +75,47 @@ class DishController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Dish $dish)
     {
-        //
+        return view('admin.dish.edit', compact('dish'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Dish $dish)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|text',
+            'price' => 'required|number',
+            'image' => 'nullable|image|mimes:jpeg,jpg,png',
+        ], [
+            'name.required' => 'Il nome del piatto è obbligatorio.',
+            'price.required' => "Il prezzo del piatto è obbligatorio.",
+            'image.image' => 'L\'immagine deve essere un file di tipo immagine',
+            'image.mimes' => 'L\'immagine deve essere un file png, jpg o jpeg',
+        ]);
+
+        $old_d_name = $dish->name;
+
+        $data = $request->all();
+
+        if (array_key_exists('image', $data)) {
+            if ($dish->image) Storage::delete($dish->image);
+            $img_url = Storage::put('dishes', $data['image']);
+            $data['image'] = $img_url;
+
+            $dish->image = $data['image'];
+        };
+
+        $dish->name = $data['name'];
+        if ($dish->description) $dish->description = $data['description'];
+        $dish->price = $data['price'];
+
+        $dish->save();
+
+        return to_route('admin.dishes.index')->with('updated-allert', "Il piatto $old_d_name è stato modificato");
     }
 
     /**
