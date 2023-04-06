d9c51ca6a691e49ea3338aaf1fdf35887d6068ee
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-04-06 17:30:24 +0200

    fixed responsive

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,7 +2,7 @@
 
     <nav class="navbar navbar-expand-md ">
         <div class="container d-flex justify-content-between">
-            <div class="collapse navbar-collapse d-flex justify-content-between align-items-center" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse d-flex justify-content-between align-items-" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <div class="navMenu d-flex align-items-center">
                     <a class="mx-3 logo d-flex align-items-center" href="{{ route('dashboard') }}">
@@ -12,7 +12,7 @@
                 </div>
                 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto d-flex justify-content-end align-items-center mt-3">
+                <ul class="navbar-nav ml-auto d-flex justify-content-end mt-3">
                     @auth
                     <!-- <a class="p-0 size me-3 hover-underline-animation" href="">showRestaurant()</a> -->
                     <a class="p-0 d-none d-md-block d-xs-none size me-3 hover-underline-animation" href="{{ route('admin.orders.index') }}">showOrders()</a>
@@ -29,13 +29,13 @@
                     @endif
                     @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white p-0 me-3" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white p-0 me-3 ciao" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
                         
                         <div class="dropdown-menu dropdown-menu-right traslate" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item text-dark" href="{{ route('admin.orders.index') }}">showOrders()</a>
-                            <a class="dropdown-item text-dark" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
+                            <a class="dropdown-item text-dark d-sm-block d-md-none" href="{{ route('admin.orders.index') }}">showOrders()</a>
+                            {{-- <a class="dropdown-item text-dark" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a> --}}
                             <a class="dropdown-item text-dark" href="{{ url('profile') }}">{{__('Profile')}}</a>
                             <a class="dropdown-item text-dark" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
