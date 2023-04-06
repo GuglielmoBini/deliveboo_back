1254adbf57da17c98f7c32383acbb09efc5ec971
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-04-06 12:06:42 +0200

    fixed route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,12 +53,12 @@
 
                         <div class="mb-4 row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-custom-secondary">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a class="btn btn-link text-custom-secondary" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
                                 </a>
                                 @endif
