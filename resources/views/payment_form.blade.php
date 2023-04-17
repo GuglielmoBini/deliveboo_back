43c15aad9f12e33759ac44e3aaa70593f77fed13
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-04-17 21:13:35 +0200

    prove andate male per colpa della mia connessione

--- resources/views/payment_form.blade.php
+++ resources/views/payment_form.blade.php
@@ -37,22 +37,32 @@
     <!-- loro -->
     <form method="post" id="payment-form" action="{{ url('/checkout') }}">
         @csrf
-        <section>
-            <label for="amount">
-                <span class="input-label">Amount</span>
-                <div class="input-wrapper amount-wrapper">
-                    <input id="amount" name="amount" type="tel" min="1" placeholder="Amount"
-                        value="10">
-                </div>
-            </label>
-    
-            <div class="bt-drop-in-wrapper">
-                <div id="bt-dropin"></div>
-            </div>
-        </section>
-    
-        <input id="nonce" name="payment_method_nonce" type="hidden" />
-        <button class="button" type="submit"><span>Test Transaction</span></button>
+
+        <label for="address">Indirizzo</label>
+        <input type="text" name="delivery_address" id="address">
+
+        <label for="name">Nome</label>
+        <input type="text" name="customer_name" id="name">
+
+        <label for="surname">Cognome</label>
+        <input type="text" name="customer_surname" id="surname">
+
+        <label for="phone-number">Numero di telefono</label>
+        <input type="text" name="customer_phone_number" id="phone-number">
+
+        <label for="email">Email</label>
+        <input type="text" name="customer_email" id="email">
+
+        <label for="card-number" id="card-number">Card Number</label>
+        <div id="card-number"></div>
+
+        <label for="cvv" id="cvv">CVV</label>
+        <div id="cvv"></div>
+
+        <label for="expiration-date" id="expiration-date">Expiration Date</label>
+        <div id="expiration-date"></div>
+
+        <input type="submit" value="Pay" disabled />
     <form>
 </div>   
 @endsection
@@ -61,51 +71,74 @@
 
 <!-- SCRIPTING -->
 @section('scripts')    
-<script src="https://js.braintreegateway.com/web/dropin/1.36.1/js/dropin.min.js"></script> 
-<script>
-        var form = document.querySelector('#payment-form');
-        var client_token = "{{ $token }}";
+<script src="https://js.braintreegateway.com/web/3.92.1/js/client.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.92.1/js/hosted-fields.min.js"></script>
+    <script>
+      var form = document.querySelector('#my-sample-form');
+      var submit = document.querySelector('input[type="submit"]');
 
-        braintree.dropin.create({
-            authorization: client_token,
-            selector: '#bt-dropin',
-            paypal: {
-                flow: 'vault'
+      braintree.client.create({
+        authorization: '{{$token}}'
+      }, function (clientErr, clientInstance) {
+        if (clientErr) {
+          console.error(clientErr);
+          return;
+        }
+
+        // This example shows Hosted Fields, but you can also use this
+        // client instance to create additional components here, such as
+        // PayPal or Data Collector.
+
+        braintree.hostedFields.create({
+          client: clientInstance,
+          styles: {
+            'input': {
+              'font-size': '14px'
+            },
+            'input.invalid': {
+              'color': 'red'
+            },
+            'input.valid': {
+              'color': 'green'
             }
-        }, function(createErr, instance) {
-            if (createErr) {
-                console.log('Create Error', createErr);
+          },
+          fields: {
+            number: {
+              container: '#card-number',
+              placeholder: '4111 1111 1111 1111'
+            },
+            cvv: {
+              container: '#cvv',
+              placeholder: '123'
+            },
+            expirationDate: {
+              container: '#expiration-date',
+              placeholder: '10/2022'
+            }
+          }
+        }, function (hostedFieldsErr, hostedFieldsInstance) {
+          if (hostedFieldsErr) {
+            console.error(hostedFieldsErr);
+            return;
+          }
+
+        //   submit.removeAttribute('disabled');
+
+          form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            hostedFieldsInstance.tokenize(function (tokenizeErr, payload) {
+              if (tokenizeErr) {
+                console.error(tokenizeErr);
                 return;
-            }
-            form.addEventListener('submit', function(event) {
-                event.preventDefault();
+              }
 
-                instance.requestPaymentMethod(function(err, payload) {
-                    if (err) {
-                        console.log('Request Payment Method Error', err);
-                        return;
-                    }
-
-                    // Add the nonce to the form and submit
-                    document.querySelector('#nonce').value = payload.nonce;
-                    form.submit();
-                });
+              // If this was a real integration, this is where you would
+              // send the nonce to your server.
+              console.log('Got a nonce: ' + payload.nonce);
             });
+          }, false);
         });
-</script>
-<script>
-        const cardInput = document.getElementById("card-number");
-        const cardNumberInput = document.getElementById("credit-card-number");
-        const expiInput = document.getElementById("expir");
-        const expirationInput = document.getElementById("expiration");
-
-        cardNumberInput.addEventListener("input", () => {
-            cardInput.value = cardNumberInput.value.replace(/\s+/g, '').replace(/(\d{4})/g, '$1 ').trim();
-
-        });
-
-        expirationInput.addEventListener("input", () => {
-            expiInput.value = expirationInput.value.replace(/(\d{2})/, '$1/').trim();
-        });
-</script>
+      });
+    </script>
 @endsection
